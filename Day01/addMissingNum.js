7b3a82e41b3b22a450e5c066b791f2fb0ff993b6
Yoonha Kim <ivryxx@gmail.com>
2022-03-14 22:01:29 +0900

    test commit

---
+++ Day01/addMissingNum.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    var answer = 0;
+    for(let i=0; i<10; i++){
+        if(!numbers.includes(i)) answer += i; 
+        // !는 논리 부정 연산자=> 피연산자의 true/false를 변환해서 반환한다.
+        // numbers가 i를 포함하지 않는다면, answer에 그 i 값들을 더해라
+    } 
+    console.log(answer);
+}
