3d8ff7bf498c90fb69c456485583170b625d094b
Yoonha Kim <ivryxx@gmail.com>
2022-04-06 23:51:23 +0900

    Create 38_numArr&words.js

---
+++ Day06/38_numArr&words.js
@@ -0,0 +1,24 @@
+//programmers.co.kr/learn/courses/30/lessons/81301
+
+https: function solution(s) {
+  let numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  var answer = s;
+
+  for (let i = 0; i < numbers.length; i++) {
+    let arr = answer.split(numbers[i]);
+    answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
